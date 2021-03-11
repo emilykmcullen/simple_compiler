1c76735b7b851eae81da4be7a728f760bcdfced4
emilykmcullen <emilycullen135@gmail.com>
2021-03-11 11:22:25 +0000

    added token class and tokentype enum

--- lex.py
+++ lex.py
@@ -1,3 +1,5 @@
+import enum
+
 class Lexer:
     def __init__(self, input):
         self.source = input + '\n' # Source code to lex as a string. Append a newline to simplify lexing/parsing the last token/statement.
@@ -5,7 +7,6 @@ class Lexer:
         self.curPos = -1    # Current position in the string.
         self.nextChar()
 
-    # Process the next character.
     def nextChar(self):
         self.curPos += 1
         if self.curPos >= len(self.source):
@@ -13,7 +14,6 @@ class Lexer:
         else:
             self.curChar = self.source[self.curPos]
 
-    # Return the lookahead character.
     def peek(self):
         if self.curPos + 1 >= len(self.source):
             return '\0'
@@ -22,14 +22,68 @@ class Lexer:
     def abort(self, message):
         pass
 		
-    # Skip whitespace except newlines, which we will use to indicate the end of a statement.
     def skipWhitespace(self):
         pass
 		
-    # Skip comments in the code.
+   
     def skipComment(self):
         pass
 
     # Return the next token.
     def getToken(self):
-        pass
+        # Check the 1st character of this token
+        # If it's a multiple character operator (eg. !=), number, identifier, or keyword then we will process the rest
+        if self.curChar == '+':
+            pass
+        elif self.curChar == '-':
+            pass
+        elif self.curChar == '*':
+            pass
+        elif self.curChar == '/':
+            pass
+        elif self.curChar == '\n':
+            pass
+        elif self.curChar == '\0':
+            pass
+        else:
+            pass
+        self.nextChar()
+
+
+
+class Token:   
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText   # The token's actual text. Used for identifiers, strings, and numbers.
+        self.kind = tokenKind   # The TokenType that this token is classified as.
+
+
+class TokenType(enum.Enum):
+	EOF = -1
+	NEWLINE = 0
+	NUMBER = 1
+	IDENT = 2
+	STRING = 3
+	# Keywords.
+	LABEL = 101
+	GOTO = 102
+	PRINT = 103
+	INPUT = 104
+	LET = 105
+	IF = 106
+	THEN = 107
+	ENDIF = 108
+	WHILE = 109
+	REPEAT = 110
+	ENDWHILE = 111
+	# Operators.
+	EQ = 201  
+	PLUS = 202
+	MINUS = 203
+	ASTERISK = 204
+	SLASH = 205
+	EQEQ = 206
+	NOTEQ = 207
+	LT = 208
+	LTEQ = 209
+	GT = 210
+	GTEQ = 211
